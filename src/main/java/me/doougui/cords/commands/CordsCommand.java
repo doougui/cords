a47ea45b8947ab2dc6e646e4c3aeea1bea775c85
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2024-07-14 11:16:28 -0300

    mention players to get their coords

--- src/main/java/me/doougui/cords/commands/CordsCommand.java
+++ src/main/java/me/doougui/cords/commands/CordsCommand.java
@@ -9,13 +9,15 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
+import java.util.Objects;
+
 public class CordsCommand implements CommandExecutor {
     Plugin plugin = Cords.getPlugin(Cords.class);
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
         if (!(sender instanceof Player)) return false;
 
-        Player player = (Player) sender;
+        Player player = getPlayer(strings, (Player) sender);
         Location location = player.getLocation();
 
         int x = (int) location.getX();
@@ -27,5 +29,14 @@ public class CordsCommand implements CommandExecutor {
         plugin.getServer().broadcastMessage(msg);
 
         return true;
+    }
+
+    private Player getPlayer(String[] mentions, Player sender) {
+        if (mentions.length == 0) return sender;
+
+        Player mentionedPlayer = plugin.getServer().getPlayer(mentions[0]);
+        if (mentionedPlayer != null) return mentionedPlayer;
+
+        return sender;
     }
 }
